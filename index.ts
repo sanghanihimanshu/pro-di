e691137824b8398e508ece0b713cf03d3baffe98
sanghanihimanshu <himanshu.creator.info@gmail.com>
2025-02-07 17:32:48 +0530

    remove unused test import from index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 export { Binder } from "@/binder";
 export { Factory, Inject, OptionalInject, Service } from "@/decorators";
-// src/index.ts
-import "./test/testprop"
 export { GlobalDIContainer } from "@/di-container";
 export { defineModule } from "@/module";
 export {
