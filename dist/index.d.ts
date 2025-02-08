ed886a5aae065564ba153e5d957023c850e11a5d
sanghanihimanshu <himanshu.creator.info@gmail.com>
2025-02-08 12:59:27 +0530

    remove unused dist files and declarations from the project

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { Binder } from "./src/binder.js";
-export { Factory, Inject, OptionalInject, Service } from "./src/decorators.js";
-import "./test/testprop";
-export { GlobalDIContainer } from "./src/di-container.js";
-export { defineModule } from "./src/module.js";
-export { expressRequestScopeMiddleware, honoRequestScopeMiddleware, getFromRequest, getFromHono, } from "./src/integrations.js";
--- dist/src/module.d.ts
+++
@@ -1,6 +0,0 @@
-import { Binder } from "./binder.js";
-/**
- * defineModule registers a set of dependency bindings with the global container.
- * @param builder - A function that receives a Binder for registering bindings.
- */
-export declare function defineModule(builder: (bind: Binder) => void): void;
